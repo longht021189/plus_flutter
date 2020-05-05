b53c257b41c6d11ad03f0e23492b0d0d8308682c
Thanh Long <longht021189@gmail.com>
2020-05-05 22:55:12 +0700

    update

--- packages/widgets/lib/proxy/MaterialAppProxy.dart
+++ packages/widgets/lib/proxy/MaterialAppProxy.dart
@@ -3,32 +3,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class MaterialAppProxy extends SingleChildRenderObjectWidget {
-
-  final StreamSink<Size> streamSize;
-
-  MaterialAppProxy({
-    Key key,
-    @required MaterialApp child,
-    this.streamSize
-  }): super(key: key, child: child);
-
-  @override
-  RenderObject createRenderObject(
-      BuildContext context) => _MaterialAppProxyElement(null, streamSize);
-}
-
-class _MaterialAppProxyElement extends RenderProxyBox {
-
+class _StreamWrapper {
   final StreamSink<Size> _streamSize;
   bool _isDone = false;
 
-  _MaterialAppProxyElement([
-    RenderBox child,
-    StreamSink<Size> streamSize
-  ]) : _streamSize = streamSize, super(child) {
-    watchDone();
-  }
+  _StreamWrapper(this._streamSize);
 
   Future watchDone() async {
     if (_streamSize == null) {
@@ -39,12 +18,63 @@ class _MaterialAppProxyElement extends RenderProxyBox {
     }
   }
 
+  void updateSize(Size size) {
+    if (!_isDone) {
+      _streamSize.add(size);
+    }
+  }
+}
+
+class MaterialAppProxy extends SingleChildRenderObjectWidget {
+
+  final Function(Size) callback;
+
+  MaterialAppProxy._internal({
+    Key key,
+    @required MaterialApp child,
+    this.callback
+  }) : super(key: key, child: child);
+
+  factory MaterialAppProxy({
+    Key key,
+    @required MaterialApp child,
+    Function(Size) callback
+  }) {
+    return MaterialAppProxy._internal(
+      key: key, child: child, callback: callback);
+  }
+
+  factory MaterialAppProxy.withStream({
+    Key key,
+    @required MaterialApp child,
+    StreamSink<Size> streamSize
+  }) {
+    final callback = _StreamWrapper(streamSize);
+
+    return MaterialAppProxy._internal(
+      key: key, child: child, callback: callback.updateSize);
+  }
+
+  @override
+  RenderObject createRenderObject(
+      BuildContext context) => _MaterialAppProxyElement(null, callback);
+}
+
+class _MaterialAppProxyElement extends RenderProxyBox {
+
+  final Function(Size) _callback;
+
+  _MaterialAppProxyElement([
+    RenderBox child,
+    Function(Size) callback
+  ]) : _callback = callback, super(child);
+
   @override
   void performLayout() {
     super.performLayout();
 
-    if (_streamSize != null && !_isDone) {
-      _streamSize.add(size);
+    if (_callback != null) {
+      _callback(size);
     }
   }
 }
